cb1d0cbf5da68299c6d9e1e86ff5e8a15f39329e
savcenkodar <79707700+savcenkodar@users.noreply.github.com>
2022-01-12 11:13:58 +0200

    hello world 2

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppProject
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine("hello world");
         }
     }
 }
